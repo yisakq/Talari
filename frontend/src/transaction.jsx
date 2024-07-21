22be7df368f4e2d45ef54b4a45c5d4a4b4b4bcad
yisakq <myesak@gmail.com>
2024-07-21 11:52:06 -0700

    modification of vercel.json

--- frontend/src/transaction.jsx
+++ frontend/src/transaction.jsx
@@ -26,7 +26,7 @@ function App() {
   
   async function getNameAndBalance() {
     try {
-      const res = await axios.get(`https://talari-server.vercel.app/`, {
+      const res = await axios.get(`https://talari-server.vercel.app/getNameAndBalance`, {
         params: { userAddress: address },
       });
   
