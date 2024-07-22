2a5025dedf1409fa48073241ee2d5972e5e8af38
yisakq <myesak@gmail.com>
2024-07-22 11:35:48 -0700

    modification of vercel.json

--- frontend/src/wallet.js
+++ frontend/src/wallet.js
@@ -2,7 +2,7 @@ export const openMetaMaskApp = () => {
     const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
   
     if (isMobile) {
-      const deepLink = 'https://metamask.app.link/dapp/YOUR_DAPP_URL';
+      const deepLink = `https://metamask.app.link/dapp/talari.vercel.app`;
       window.open(deepLink, '_self');
     }
   };
