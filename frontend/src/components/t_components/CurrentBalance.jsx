396342c345ebd8afdf848b1540929ce21a680f88
yisakq <myesak@gmail.com>
2024-07-24 11:29:51 -0700

    modification of smartcontract

--- frontend/src/components/t_components/CurrentBalance.jsx
+++ frontend/src/components/t_components/CurrentBalance.jsx
@@ -3,7 +3,7 @@ import { Card } from "antd";
 
 function CurrentBalance({dollars}) {
   return (
-    <Card title="Current Balance" style={{ width: "100%" }}>
+    <Card title="Current Balance" style={{ width: "100%",marginTop:"50px" }}>
       <div className="currentBalance">
         <div style={{ lineHeight: "70px" }}>${dollars}</div>
         <div style={{ fontSize: "20px" }}>Available</div>
